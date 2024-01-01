79d73ba0a5eae020aeda2536ef3a510e9b3ba298
Egon Willighagen <egon.willighagen@gmail.com>
2024-01-01 10:16:58 +0100

    Rewritten to use a RDF toolkit (via Bacting)

--- extractBioschemas.groovy
+++ extractBioschemas.groovy
@@ -1,10 +1,10 @@
-// Copyright (c) 2022  Egon Willighagen <egon.willighagen@gmail.com>
+// Copyright (c) 2022-2024  Egon Willighagen <egon.willighagen@gmail.com>
 //
 // GPL v3
 
-@Grab(group='io.github.egonw.bacting', module='managers-rdf', version='0.3.3')
-@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.3.3')
-@Grab(group='io.github.egonw.bacting', module='net.bioclipse.managers.jsoup', version='0.3.3')
+@Grab(group='io.github.egonw.bacting', module='managers-rdf', version='0.5.0')
+@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.5.0')
+@Grab(group='io.github.egonw.bacting', module='net.bioclipse.managers.jsoup', version='0.5.0')
 
 bioclipse = new net.bioclipse.managers.BioclipseManager(".");
 rdf = new net.bioclipse.managers.RDFManager(".");
@@ -43,31 +43,39 @@ SELECT ?dataset ?url ?name ?license ?description WHERE {
 
 results = rdf.sparql(kg, sparql)
 
-println "@prefix dc:    <http://purl.org/dc/elements/1.1/> ."
-println "@prefix dct:   <http://purl.org/dc/terms/> ."
-println "@prefix foaf:  <http://xmlns.com/foaf/0.1/> ."
-println "@prefix rdfs:  <http://www.w3.org/2000/01/rdf-schema#> ."
-println "@prefix sbd:   <https://www.sbd4nano.eu/rdf/#> ."
-println "@prefix xsd:   <http://www.w3.org/2001/XMLSchema#> ."
-println "@prefix void:    <http://rdfs.org/ns/void#> ."
-println ""
-println "<https://nanocommons.github.io/datasets/>"
-println " a                    void:DatasetDescription ;"
-println " dc:source            <https://nanocommons.github.io/datasets/> ;"
-println " dct:title            \"Overview of open datasets released by NanoSafety Cluster projects\"@en ;"
-println " foaf:img             <https://upload.wikimedia.org/wikipedia/commons/e/e1/NanoCommons-Logo-Large_-_White_Circle_01.png> ;"
-println " dct:license          <http://creativecommons.org/publicdomain/zero/1.0/> ."
-println ""
+store = rdf.createInMemoryStore(false)
+rdf.addPrefix(store, "dc", "http://purl.org/dc/elements/1.1/")
+rdf.addPrefix(store, "dct", "http://purl.org/dc/terms/")
+rdf.addPrefix(store, "foaf", "http://xmlns.com/foaf/0.1/")
+rdf.addPrefix(store, "rdfs", "http://www.w3.org/2000/01/rdf-schema#")
+rdf.addPrefix(store, "sbd", "https://www.sbd4nano.eu/rdf/#")
+rdf.addPrefix(store, "xsd", "http://www.w3.org/2001/XMLSchema#")
+rdf.addPrefix(store, "void", "http://rdfs.org/ns/void#")
+
+propType    = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
+propSource  = "http://purl.org/dc/elements/1.1/source"
+propLicense = "http://purl.org/dc/terms/license"
+
+metaIRI = "https://nanocommons.github.io/datasets/"
+rdf.addObjectProperty(store, metaIRI, propType, "http://rdfs.org/ns/void#DatasetDescription")
+rdf.addObjectProperty(store, metaIRI, propSource, "https://nanocommons.github.io/datasets/")
+rdf.addPropertyInLanguage(store, metaIRI, "http://purl.org/dc/terms/title", "Overview of open datasets released by NanoSafety Cluster projects", "en")
+rdf.addObjectProperty(store, metaIRI, "http://xmlns.com/foaf/0.1/img", "https://upload.wikimedia.org/wikipedia/commons/e/e1/NanoCommons-Logo-Large_-_White_Circle_01.png")
+rdf.addObjectProperty(store, metaIRI, propLicense, "http://creativecommons.org/publicdomain/zero/1.0/")
 
 for (i=1;i<=results.rowCount;i++) {
-  println "<${results.get(i, "dataset")}> a sbd:Dataset ;"
-  println "  dc:source <https://nanocommons.github.io/datasets/> ;"
-  if (results.get(i, "name") != null) println "  rdfs:label \"${results.get(i, "name")}\"@en ;"
-  if (results.get(i, "description") != null) {
-    description = results.get(i, "description").replaceAll("\"", "\\\"") 
-    println "  dc:description \"${description}\"@en ;"
-  }
-  if (results.get(i, "license") != null) println "  dct:license <${results.get(i, "license")}> ;"
-  println "  foaf:page <${results.get(i, "url")}> ."
+  datasetIRI = results.get(i, "dataset")
+  rdf.addObjectProperty(store, datasetIRI, propType, "https://www.sbd4nano.eu/rdf/#Dataset")
+  rdf.addObjectProperty(store, datasetIRI, propSource, metaIRI)
+  if (results.get(i, "name") != null)
+    rdf.addPropertyInLanguage(store, datasetIRI,
+      "http://www.w3.org/2000/01/rdf-schema#label", results.get(i, "name"), "en")
+  if (results.get(i, "description") != null)
+    rdf.addPropertyInLanguage(store, datasetIRI,
+      "http://purl.org/dc/elements/1.1/description", results.get(i, "description"), "en")
+  if (results.get(i, "license") != null) rdf.addObjectProperty(store, datasetIRI, propLicense, results.get(i, "license"))
+  rdf.addObjectProperty(store, datasetIRI, "http://xmlns.com/foaf/0.1/page", results.get(i, "url"))
   println ""
 }
+
+println rdf.asTurtle(store)
